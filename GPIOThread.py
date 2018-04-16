a47adef792f6059890bdbc91e1e31f11457544a3
Alex Tsai <alext5@vt.edu>
2018-04-16 16:34:53 -0400

    updated GPIO Thread code

--- GPIOThread.py
+++ GPIOThread.py
@@ -7,9 +7,10 @@
 ############################################################################################################
 
 import RPi.GPIO as GPIO
-import sys
+import sys, time
 from enum import Enum
 from PyQt5.QtCore import QThread, pyqtSlot, pyqtSignal, Qt
+from stateMachine import StateMachine as states
 
 ACC_SWITCH = 24
 IGN_SWITCH = 25
@@ -17,52 +18,38 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(ACC_SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(IGN_SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-class State(Enum):
-	IDLE = 0
-	ACCON = 1
-	MCON = 2
-	PRECHARGE = 3
-        PRECHARGE_DONE = 4
-	ESTOP = 10
-
 class GPIOThread(QThread):
         accessoryPress = pyqtSignal(int)
         ignitionPress = pyqtSignal(int)
-        precharge = pyqtSignal(int)
-        estop = pyqtSignal(int)
-        raceMode = pyqtSignal()
+        #estop = pyqtSignal(int)
 	def __init__(self):
                 self.ACC_FLAG = False
                 self.IGN_FLAG = False
-                self.vsmState = 0
-		self.currentState = State.IDLE
 		QThread.__init__(self)
 	def run(self):
-		print("State Machine Thread Started", self.currentThread())
-		print("Current State: IDLE")
+		print("GPIO Thread Started", self.currentThread())
 		while(True):
-			if self.currentState.name == State.IDLE:
+			if states.current_state == 'IDLE':
 				if GPIO.input(ACC_SWITCH) and !(self.ACC_FLAG):
-					print("Current State: Accessory On")
                                         self.accessoryPress.emit(1)
-					self.currentState = State.ACCON
                                         self.ACC_FLAG = True
-			elif self.currentState.name == State.ACCON:
+				elif not GPIO.input(ACC_SWITCH) and self.ACC_FLAG:
+					self.accessoryPress.emit(0)
+					self.ACC_FLAG = False
+			elif states.current_state == 'ACC_ON':
 				if GPIO.input(IGN_SWITCH) and !(self.IGN_FLAG):
-					print("Current State: Motor Controller On") 
-                                        self.ignitionPress.emit(2)
-					self.currentState = State.MCON
+                                        self.ignitionPress.emit(1)
                                         self.IGN_FLAG = True
-			elif self.currentState.name == State.MCON:
-				print("Current State: Pre-charge begins")
-                                self.precharge.emit(3)
-				self.currentState = State.PRECHARGE
-                                print("Current State: Pre-charging")
-			elif self.currentState.name == State.PRECHARGE and self.vsmState == 2:
-                                if self.vsmState == 3:
-                                    self.currentState == State.PRECHARGE_DONE
-                        elif self.currentState.name == State.PRECHARGE_DONE
-                                self.raceMode.emit()
-       @pyqtSlot(int)
-       def vsmUpdate(self, value):
-           self.vsmState = value
+				elif not GPIO.input(IGN_SWITCH) and self.IGN_FLAG:
+					self.ignitionPress.emit(0)
+					self.IGN_FLAG = False
+			time.sleep(.1)
+			#elif self.currentState.name == State.PRECHARGE and self.vsmState == 2:
+                                #if self.vsmState == 3:
+                                    #self.currentState == State.PRECHARGE_DONE
+                        #elif self.currentState.name == State.PRECHARGE_DONE
+                                #self.raceMode.emit()
+		self.exec()
+       #@pyqtSlot(int)
+       #def vsmUpdate(self, value):
+           #self.vsmState = value
