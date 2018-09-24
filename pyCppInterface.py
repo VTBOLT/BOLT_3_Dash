1f38b2950a0abe34cbf2bac98cfb8be2425256a3
Khang Lieu <khangdl@vt.edu>
2018-09-23 20:50:36 -0700

    Data should print, need to test before moving further

--- pyCppInterface.py
+++ pyCppInterface.py
@@ -3,17 +3,28 @@
 # Author: Khang Lieu
 # Written: Fall 2018
 
+import can
+import socket
+can.rc['interface'] = 'socketcan'
+can.rc['channel'] = 'vcan0'
+can.rc['bitrate'] = 500000
+from can.interface import Bus
 
-class CanReader
+bus = Bus()
 
-    bool abort
 
-    int soc
+
+class CanReader():
+
+    abort = False
+
+    soc = 0
 
     def __init__(self):
-    
+        self.abort = False
+        self.run()
 
-    def initialize(self):
+
 
     def run(self):
 
@@ -40,18 +51,65 @@ class CanReader
         inverter_enable_state = 0
         direction_state = 0
 
-        while(this.abort == true)
+        while 1:
+            for msg in bus:
+                print(type(msg))
+                if(msg.arbitration_id == 0xA0):
+                    #MC Temperatures
+                    print(msg.data)
+                if(msg.arbitration_id == 0xA2):
+                    #MC Temperatures
+                    print(msg.data)
 
-    def openPort(self,int port):
-    
-    def max(a, b, c, d):
-        int max = 0
-        if(a >= max)
+                if(msg.arbitration_id == 0xA5):
+                    #Motor Position
+                    print(msg.data)
+
+                if(msg.arbitration_id == 0x180):
+                    #BMS Voltages
+                    #High low cell data
+                    print(msg.data)
+
+                if(msg.arbitration_id == 0x181):
+                    #BMS Temperatures
+                    print(msg.data)
+
+                if(msg.arbitration_id == 0x182):
+                    #BMS Isolations
+                    print(msg.data)
+
+                if(msg.arbitration_id == 0x111):
+                    #BMS DCL
+                    print(msg.data)
+
+                if(msg.arbitration_id == 0x183):
+                    #BMS Information
+                    print(msg.data)
+
+                if(msg.arbitration_id == 0xAA):
+                    #Internal State of MC
+                    print(msg.data)
+
+                if(msg.arbitration_id == 0xAB):
+                    #MC Errors
+                    print(msg.data)
+
+
+       
+
+
+
+    def max(self,a, b, c, d):
+        max = 0
+        if(a >= max):
             max = a
-        else if(b >= max)
+        elif(b >= max):
             max = b
-        else if(c >= max)
+        elif(c >= max):
             max = c
-        else if(d >= max)
+        elif(d >= max):
             max = d
         return max
+
+
+CanReader()
